3f1845679267de8b28aa3cff961aef23721c5edf
Alexis Le Provost <alexis.leprovost@outlook.com>
2020-08-14 16:55:29 +0200

    format on save Rust codes

--- +rust.el
+++ +rust.el
@@ -1,4 +1,5 @@
 ;;; ~/dev/doom-emacs-private/+rust.el -*- lexical-binding: t; -*-
 
 (after! rustic
-  (setq rustic-lsp-server 'rust-analyzer))
+  (setq rustic-lsp-server 'rust-analyzer
+        rustic-format-on-save t))
