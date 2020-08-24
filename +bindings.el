f8d92a9cf357a2fb693fce4f75221f255941f00f
Alexis Le Provost <alexis.leprovost@outlook.com>
2020-08-24 17:41:13 +0200

    Update bindings

--- +bindings.el
+++ +bindings.el
@@ -2,9 +2,7 @@
 
 (map! :n "M-r" #'evil-multiedit-match-all
       ;; Avoid to strike repeatedly the leader key
-      :n "w" evil-window-map
-      :leader
-      :desc "imenu" "oi" #'imenu-list-toggle)
+      :n "w" evil-window-map)
 
-(use-package! treemacs
+(after! treemacs
   :config (define-key! evil-treemacs-state-map "w" evil-window-map))
