ab8883eac9691d4b0041bbb24d9c5e1371b5b6fd
Alexis Le Provost <alexis.leprovost@outlook.com>
2021-01-12 13:49:25 +0100

    Add SPC t i shortcut to toggle inlay hint display

--- +bindings.el
+++ +bindings.el
@@ -2,6 +2,14 @@
 
 (map! :n "M-r" #'evil-multiedit-match-all
       ;; Avoid to strike repeatedly the leader key
+      :n "w" evil-window-map
+      :n "]" next-error
+      :n "[" previous-error
+      :leader
+      :after lsp-mode
+      :desc "Inlay hints" "t i" #'lsp-rust-analyzer-inlay-hints-mode)
+
+(map! :map magit-mode-map
       :n "w" evil-window-map)
 
 (after! treemacs
