d3d631027a145d2e10b8c057f22bfb67a47ab920
Alexis Le Provost <alexis.leprovost@outlook.com>
2021-01-12 14:13:56 +0100

    Adds *w O* shortcut to close all other windows.

--- +bindings.el
+++ +bindings.el
@@ -10,5 +10,8 @@
 (map! :map (magit-mode-map magit-diff-mode-map)
       :n "w" evil-window-map)
 
+(map! :map evil-window-map
+      :desc "Kill other windows" "O" #'delete-other-windows)
+
 (after! treemacs
   :config (define-key! evil-treemacs-state-map "w" evil-window-map))
