be68b8927221e814ad8d20ce35585e01458d5c73
Alexis Le Provost <alexis.leprovost@outlook.com>
2022-11-30 15:23:41 +0100

    Remove evil-window-map keybinding shortcut

--- +bindings.el
+++ +bindings.el
@@ -1,14 +1,12 @@
 ;;; ~/dev/doom-emacs-private/+bindings.el -*- lexical-binding: t; -*-
 
-(map! :n "M-r" #'evil-multiedit-match-all
-      ;; Avoid to strike repeatedly the leader key
-      :n "w" evil-window-map)
+(map! :n "M-r" #'evil-multiedit-match-all)
 
 (map! :map evil-window-map
       :desc "Kill other windows" "O" #'delete-other-windows)
 
 (map! :map (magit-mode-map magit-diff-mode-map)
-      :n "w" evil-window-map
+      ;; :n "w" evil-window-map
       :n "g r" 'magit-refresh
       :n "g R" 'magit-refresh-all
       :n "<tab>" 'magit-section-toggle)
@@ -16,11 +14,6 @@
 (map! :map rustic-mode-map
       :leader
       :desc "Inlay hints" "ch" #'lsp-rust-analyzer-inlay-hints-mode)
-
-(after! treemacs
-  :config
-  (define-key! evil-treemacs-state-map "w" evil-window-map)
-  (define-key! evil-treemacs-state-map "M-w" 'treemacs-set-width))
 
 (after! lsp-ui
   (map!
